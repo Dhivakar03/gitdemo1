bd4164704fe91d46181bda1300bf0c3d2091c8eb
Dhivakar03 <msdhivakar92@gmail.com>
2023-09-14 16:11:38 +0530

    update.bun

---
+++ bun.js
@@ -0,0 +1,9 @@
+// Define a function called "sayHello"
+function sayHello() {
+    // Inside the function, print a message to the console
+    console.log("Hello, World!");
+  }
+  
+  // Call the "sayHello" function to execute it
+  sayHello();
+  
